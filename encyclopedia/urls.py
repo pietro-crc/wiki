c8d5cddbde2c308d9713f494f8e8a6af6e4dc4e4
pietro-crc <135025750+pietro-crc@users.noreply.github.com>
2024-01-10 22:41:01 +0100

    "first commit"

---
+++ encyclopedia/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+
+    path("wiki/<str:title>", views.util.get_entry, name="get entry"),
+]
